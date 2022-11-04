d86c825743a24d195b2d42ca9036cbe4f83abafb
dylanquaale <dylanquaale33@gmail.com>
2022-11-04 11:51:48 -0500

    added the first screen user will seebutton click

--- script.js
+++ script.js
@@ -2,14 +2,13 @@
 var generateBtn = document.querySelector("#generate");
 
 function randomInt(min, max) {
-    if(!max) {
+    if (!max) {
         max = min
         min=0
     }
 }
-
 var rand = Math.random()
-    return Math.floor(min*(1 - rand) + rand*max)
+ return Math.floor(min*(1 - rand) + rand*max)
 //gives user and random position in the list 
 function getRandomItem(list) {
     return list[randomInt(list.length)]
@@ -17,7 +16,19 @@ function getRandomItem(list) {
 
 function generatePassword() {
 
-    
+    var userInput = window.prompt("How many characters would you like in your password?")
+    //parse in will try to give a string a number value 
+    var passwordLength = pareseInt(userInput)
+    // tells us that is not a number
+    if(isNaN(passwordLength)) {
+        window.alert("oops not a number")
+        return
+    }
+
+
+
+
+
 
 
 
